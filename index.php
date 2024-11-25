ef399243727022911deb335e27e4d4018ab87f4d
johnwayneds <ivan.sesar222@gmail.com>
2024-11-25 09:38:16 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+// Varijable za naslov i poveznicu
+$title = "Da Vincijev kod";
+$link = "https://hr.wikipedia.org/Da_Vincijev_kod";
+?>
+
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+    <title><?php echo $title; ?></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Da Vincijev kod je kriminalistički triler američkog pisca Dana Browna.">
+    <meta name="keywords" content="Da Vinci, kod, triler, knjiga, Dan Brown">
+</head>
+<body>
+    <h1><?php echo $title; ?></h1>
+    <p>Da Vincijev kod je kriminalistički triler američkog pisca Dana Browna.</p>
+    <p>
+        <a href="<?php echo $link; ?>" target="_blank">
+            Pročitajte više na Wikipediji
+        </a>
+    </p>
+    
+    <?php
+    // index2.php
+    ?>
+    
+</body>
+</html>
